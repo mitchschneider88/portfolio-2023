f457309329c7453763295e59a42b14b9a98121b6
mitchschneider88 <117307463+mitchschneider88@users.noreply.github.com>
2024-12-16 14:34:31 -0600

    updated bio

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ const descriptions = [
     "brat tour: music direction", // 15 brat
     "sweat tour: music direction for Charli XCX & Troye Sivan", // 16 sweat
     "GCDS SS25: music arrangement & editing", // 17 GCDS
-    "music direction / broadcast mix supervision" // 18 SNL50
+    "music direction / broadcast mix supervision", // 18 SNL50
+    "music direction / programming" // 19 stgte
 ];
 
 
